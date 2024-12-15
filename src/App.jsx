0b0a65af59fd6d54c076d3f9556c54c1e006fd25
Sherzod-Tohirov <sherzodtohirov862@gmail.com>
2024-12-15 10:52:10 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from 'react-bootstrap';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+
+  return (
+    <>
+       Hello World      
+       <Button className="me-auto">dwdwdwdwd</Button>
+    </>
+  )
+}
+
+export default App
